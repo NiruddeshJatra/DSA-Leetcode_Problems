8064e1619cc7913fe1a9e432cdb825bbb36ce04f
Nasiful Alam <nasifulalam1212@gmail.com>
2024-08-19 11:29:33 +0600

    Create P108 - Middle of The Linked List.py

---
+++ Linked List/P108 - Middle of The Linked List.py
@@ -0,0 +1,15 @@
+# Each time, slow go 1 steps while fast go 2 steps.
+# When fast arrives at the end, slow will arrive right in the middle.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
